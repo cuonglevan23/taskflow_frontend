68739b8256d04ed839c729d56fc53a14b82ca8b3
anhductk74 <anhduc120102@gmail.com>
2025-07-23 22:35:32 +0700

    feat: add HeaderMyTask component and integrate it into MyTaskLayout; create new Board page

--- src/app/mytask/layout.tsx
+++ src/app/mytask/layout.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import MainLayout from '@/components/layout/MainLayout'
+import HeaderMyTask from '@/app/mytask/components/header_mytask'
 
 const MyTaskLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <MainLayout>
+        <HeaderMyTask/>
         {children}
       </MainLayout>
     </div>
